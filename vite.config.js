268c49535a88d09e7256ac04442ab39e37ffb0d1
shkorlukhanov-sd <stasskorluhanov2019@mail.ru>
2026-06-27 09:10:15 +0700

    делпой

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/shop-games/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
